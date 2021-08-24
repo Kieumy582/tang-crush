ec90924996b503ca2be4d1754bf43cff7c7b2c49
Kieumy582 <89455931+Kieumy582@users.noreply.github.com>
2021-08-24 17:43:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Em pé ơi em pé à!',
+    introDesc: `Nay là ngày gì í nhở
+    Có phải là ngày đặc biệt gì hong taaa`,
+    btnIntro: 'hihi <3',
+    title: 'Em gì ơi, em đánh rơi người yêu này 😙',
+    desc: 'Từ thích thích thành yêu yêu rồi thương thương ',
+    btnYes: 'Thì là em cá yêu pé tró ạ <33',
+    btnNo: 'Em yêu pé từ cái nhìn đầu tiên ó :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu pé zạ <3',
+    btnReply: 'Gửi cho pé nà <3',
+    reply: 'Yêu thì yêu mà không yêu thì thương <33333333',
+    mess: 'Chùi uiiiii 🥰. Yêu em pé quá iiii 😘😘',
+    messDesc: 'Nào gặp pé bao em đi ăn nhó',
+    btnAccept: 'Zạaaaaa thích nhắm thích nhắm  <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
